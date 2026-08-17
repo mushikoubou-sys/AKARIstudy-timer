c4a28b487e5116e88e3414a73db8c0c703c5859d
mushikoubou-sys <mushikoubou@gmail.com>
2026-08-17 16:24:18 +0900

    タイマー修正用のキャッシュを更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mikabu-timer-v11';
+const CACHE='mikabu-timer-v12';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./firebase-config.js','./cloud-sync.js','./manifest.webmanifest','./assets/mikabu-mascot.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
